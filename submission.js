cb1ad5c79889fbc55013212bb0f8d4f0b3c4a119
Avandahm <112453899+Avandahm@users.noreply.github.com>
2022-10-02 11:54:25 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,6 +1,5 @@
 //question 1 
 const findSum = function(array) { 
-  array = [10, 234, 356, 5, 3, 1, 3, 4];
   let total = 0;
   for (let i = 0; i < array.length; i++) {
       total += array[i];
@@ -10,28 +9,27 @@ const findSum = function(array) {
 
 //question 2
 const findFrequency = function(array) {
-  array = ['a', 'a', 'b', 'b', 'c', 'd', 'd', 'd'];
   let result = {
-      most: '',
-      least: ''
-  };
-  let bestCount = 0, leastCount = array.length;
-  for (let i = 0; i < array.length; i++) {
-      let count = 0;
-      for (let j = 0; j < array.length; j++) {
-          if (array[i] === array[j])
-              ++count;
-      }
-      if (count > bestCount) {
-          bestCount = count;
-          result.most = array[i];
-      }
-      if (count < leastCount) {
-          leastCount = count;
-          result.least = array[i];
-      }
-  }
-  return result;
+    most: '',
+    least: ''
+};
+let best_count = 0, least_count = array.length;
+for (let i = 0; i < array.length; i++) {
+    let count = 0;
+    for (let j = 0; j < array.length; j++) {
+        if (array[i] === array[j])
+            ++count;
+    }
+    if (count > best_count) {
+        best_count = count;
+        result.most = array[i];
+    }
+    if (count < least_count) {
+        least_count = count;
+        result.least = array[i];
+    }
+}
+return result;
 }
 
 // // question 3
@@ -50,7 +48,6 @@ const isPalindrome = function(str) {
 
 //question 4
 const largestPair = function(array) {
-  array= [1,3,5,8];
   var max=-Infinity;  
   for (var i=0;i<array.length-1;i++){
       max = array[i]*array[i+1]>max ?array[i]*array[i+1]:max;  
@@ -60,34 +57,36 @@ const largestPair = function(array) {
 
 // question 5
 const removeParenth = function(str){
-  str=  ('codingis(not)fun') 
-  str = str.replace('(not)','');
-  
-  return str
+  var s= str 
+  var result="" 
+  for(var i=0;i<s.length;i++){
+    if(!(s[i]==='(' || s[i]===')')) 
+   result+=s[i] 
+  }
+  return result 
 }
+
 
 // // question 6
 const scoreScrabble = function(str){
   let sum=0 
-  str = ('quintessential');
-  let s={   
-      1 : ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
-      2: ['d', 'g'],
-      3 :[ 'b', 'c', 'm', 'p'],
-      4 : ['f', 'h', 'v', 'w', 'y'],
-      5 : ['k'],
-      8 : ['j', 'x'],
-      10 : ['q', 'z'],
-  }
-  let list=Object.entries(s)  
- for(let i in str){  
-   for(let j in list){ 
-     if(list[j][1].includes(str[i])){  
-       sum+=Number(list[j][0])  
+    temp= str 
+    let s={
+        1 : ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+        2: ['d', 'g'],
+        3 :[ 'b', 'c', 'm', 'p'],
+        4 : ['f', 'h', 'v', 'w', 'y'],
+        5 : ['k'],
+        8 : ['j', 'x'],
+        10 : ['q', 'z'],
+    }
+    let list=Object.entries(s)  
+   for(let i in temp){  
+     for(let j in list){ 
+       if(list[j][1].includes(temp[i])){  
+         sum+=Number(list[j][0])  
+       }
      }
    }
- }
-  return sum  
+    return sum  
 }
-
-
